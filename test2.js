cb1c722a622700b96a2e15e0609519acc9cfbe7d
wuyingjie <13916764149@163.com>
2016-06-23 17:31:29 +0800

    简单指令测试2

---
+++ test2.js
@@ -0,0 +1,69 @@
+/**
+ * Created by jackShall on 2016/6/23.
+ */
+var vm = new Vue({
+   el:'#example' ,
+    data:{
+        ok:false
+    },
+    methods:{
+        trun:function(){
+            this.ok=!this.ok;
+        }
+    }
+});
+var vm1 = new Vue({
+    el:'#example-1',
+    data:{
+        items:[
+            {id:1,text:'java'},
+            {id:2,text:'c'},
+            {id:3,text:'c++'},
+            {id:4,text:'php'}
+        ],
+        object: {
+            FirstName: 'John',
+            LastName: 'Doe',
+            Age: 30
+        }
+    },
+    methods:{
+        setArr:function(index){
+            //this.items.$set(index,{text:'这里是修改过的值'});//true
+            //this.items[index].text='这里是修改过的值';  //true
+            //this.items[index]= {};                      //fasle
+            //this.items.$set(index,{});                  //true
+
+            //这个方法参数是数组中的一个元素，并非索引
+            this.items.$remove(this.items[index]);
+            //var temp = this.items.indexOf(index);
+            //if (temp !== -1) {
+            //    this.items.splice(temp, 1)
+            //}
+        },
+        print:function(){
+            console.log(this.items.toString());
+        }
+    }
+});
+//vm1.items[0] = {}; //true
+vm1.items.$remove(0);
+
+var vm2 = new Vue({
+   el:'#example-2',
+    data:{
+        name:'Vue.js'
+    },
+    methods:{
+        greet:function(e){
+            alert('hello'+this.name+"!");
+            alert(e.target.tagName);
+        },
+        stop:function(){
+            alert('阻止冒泡事件');
+        },
+        noSubmit:function (){
+            console.log("表单提交");
+        }
+    }
+});
